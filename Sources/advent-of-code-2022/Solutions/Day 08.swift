b57aaf351eba23dba1799785a2b0cf4135483669
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-12-08 16:54:58 +0800

    feat: add day 8

--- Sources/advent-of-code-2022/Solutions/Day 08.swift
+++ Sources/advent-of-code-2022/Solutions/Day 08.swift
@@ -6,11 +6,135 @@
 //
 
 class Day8 {
-    func solve1(input _: String) -> Int {
-        0
+    func solve1(input: String) -> Int {
+        let list = handleInput(input)
+        return findVisible(list: list)
     }
 
-    func solve2(input _: String) -> Int {
-        0
+    func solve2(input: String) -> Int {
+        let list = handleInput(input)
+        return findBestViewingScore(list: list)
+    }
+
+    private func handleInput(_ input: String) -> [[Int]] {
+        input
+            .splitLines(shouldTrimWhitespacesAndNewlines: true)
+            .map { string in
+                string
+                    .map(String.init)
+                    .compactMap(Int.init)
+            }
+    }
+
+    private func findTallest(list: [[Int]], currentPosition: (idx: Int, jdx: Int)) -> (top: Int, bottom: Int, left: Int, right: Int) {
+        let (idx, jdx) = currentPosition
+
+        let tallest = Direction.allCases
+            .map { direction in
+                direction.getRange(idx: idx, jdx: jdx, length: (first: list.count, second: list[idx].count))
+                    .map { ndx in
+                        let adx = direction.navigationMode == .horizontal ? idx : ndx
+                        let bdx = direction.navigationMode == .horizontal ? ndx : jdx
+                        return list[adx][bdx]
+                    }
+                    .max()!
+            }
+
+        return (top: tallest[0], bottom: tallest[1], left: tallest[2], right: tallest[3])
+    }
+
+    private func findVisible(list: [[Int]]) -> Int {
+        var visibleCount = 0
+
+        for idx in list.indices {
+            for jdx in list[idx].indices {
+                let edgeTop = idx == 0
+                let edgeLeft = jdx == 0
+                let edgeBottom = idx == list.count - 1
+                let edgeRight = jdx == list[idx].count - 1
+
+                if edgeTop || edgeLeft || edgeBottom || edgeRight {
+                    visibleCount += 1
+                } else {
+                    let (findTop, findBottom, findLeft, findRight) = findTallest(list: list, currentPosition: (idx: idx, jdx: jdx))
+
+                    let visibleFromTop = findTop < list[idx][jdx]
+                    let visibleFromBottom = findBottom < list[idx][jdx]
+                    let visibleFromLeft = findLeft < list[idx][jdx]
+                    let visibleFromRight = findRight < list[idx][jdx]
+
+                    if visibleFromTop || visibleFromBottom || visibleFromLeft || visibleFromRight {
+                        visibleCount += 1
+                    }
+                }
+            }
+        }
+        return visibleCount
+    }
+
+    private func findBestViewingScore(list: [[Int]]) -> Int {
+        var score = 0
+        for idx in list.indices {
+            for jdx in list[idx].indices {
+                let current = findScore(list: list, idx: idx, jdx: jdx)
+                score = max(score, current)
+            }
+        }
+        return score
+    }
+
+    private func findScore(list: [[Int]], idx: Int, jdx: Int) -> Int {
+        return Direction.allCases
+            .reduce(1) {
+                $0 * findNavigationDistance(direction: $1, list: list, idx: idx, jdx: jdx)
+            }
+    }
+
+    enum Direction: CaseIterable {
+        case top, left, right, bottom
+
+        static var allCases: [Direction] {
+            return [.top, .left, .right, .bottom]
+        }
+
+        var navigationMode: NavigationMode {
+            switch self {
+            case .top, .bottom:
+                return .vertical
+            case .left, .right:
+                return .horizontal
+            }
+        }
+
+        func getRange(idx: Int, jdx: Int, length: (first: Int, second: Int)) -> [Int] {
+            switch self {
+            case .top:
+                return Array((0 ..< idx).reversed())
+            case .bottom:
+                return Array(idx + 1 ..< length.first)
+            case .left:
+                return Array((0 ..< jdx).reversed())
+            case .right:
+                return Array(jdx + 1 ..< length.second)
+            }
+        }
+    }
+
+    enum NavigationMode {
+        case horizontal
+        case vertical
+    }
+
+    private func findNavigationDistance(direction: Direction, list: [[Int]], idx: Int, jdx: Int) -> Int {
+        var distance = 0
+        for ndx in direction.getRange(idx: idx, jdx: jdx, length: (first: list.count, second: list[idx].count)) {
+            distance += 1
+            let adx = direction.navigationMode == .horizontal ? idx : ndx
+            let bdx = direction.navigationMode == .horizontal ? ndx : jdx
+            if list[adx][bdx] >= list[idx][jdx] {
+                break
+            }
+        }
+        return distance
     }
 }
--- Tests/advent-of-code-2022Tests/AOC_Tests_Day8.swift
+++ Tests/advent-of-code-2022Tests/AOC_Tests_Day8.swift
@@ -15,24 +15,24 @@ final class AOC_Tests_Day8: XCTestCase {
     func test_sample_question1() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "sample_input"))
         let answer = try XCTUnwrap(solver.solve1(input: input))
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 21)
     }
 
     func test_sample_question2() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "sample_input"))
         let answer = try XCTUnwrap(solver.solve2(input: input))
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 8)
     }
 
     func test_real_question1() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "real_input"))
         let answer = try XCTUnwrap(solver.solve1(input: input))
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 1794)
     }
 
     func test_real_question2() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "real_input"))
         let answer = solver.solve2(input: input)
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 199_272)
     }
 }
