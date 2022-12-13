59b8078102196c15b4f1915a4659018264cba5d4
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-12-13 21:08:35 +0800

    feat: add day 13

--- Sources/advent-of-code-2022/Solutions/Day 13.swift
+++ Sources/advent-of-code-2022/Solutions/Day 13.swift
@@ -6,11 +6,118 @@
 //
 
 class Day13 {
-    func solve1(input _: String) -> Int {
-        0
+    func solve1(input: String) -> Int {
+        let list = input.splitParagraphs(shouldTrimWhitespacesAndNewlines: true)
+
+        return list
+            .enumerated()
+            .map {
+                return ($0, $1.components(separatedBy: .newlines).map { unwrapList($0) })
+            }
+            .filter { compare($1[0], $1[1]) <= 0 }
+            .map { index, _ in
+                index + 1
+            }
+            .sum()
     }
 
-    func solve2(input _: String) -> Int {
-        0
+    func solve2(input: String) -> Int {
+        let list = input
+            .appending("""
+            [[2]]
+            [[6]]
+            """)
+            .splitList(separator: "\n", shouldTrimWhitespacesAndNewlines: true)
+            .dropEmpty()
+
+        return list
+            .flatMap { listItem in
+                listItem.components(separatedBy: .newlines)
+            }
+            .sorted {
+                compare(unwrapList($0), unwrapList($1)) <= 0
+            }
+            .enumerated()
+            .filter { _, value in
+                value == "[[2]]" || value == "[[6]]"
+            }
+            .map { index, _ in
+                index + 1
+            }
+            .reduce(1) { $0 * $1 }
+    }
+
+    private func unwrapList(_ value: String) -> [String] {
+        var copy = value
+        copy.consumeFirst()
+        _ = copy.popLast()
+
+        var list = copy.splitList(separator: ",", shouldTrimWhitespacesAndNewlines: false)
+        var output = [String]()
+
+        var temp: [String] = []
+        var signal = 0
+
+        while let current = list.popFirst() {
+            if current.contains("[") {
+                signal += current.filter { $0 == "[" }.count
+            }
+
+            if signal > 0 {
+                temp.append(current)
+            }
+
+            if current.contains("]") {
+                signal -= current.filter { $0 == "]" }.count
+                if signal == 0 {
+                    output.append(temp.joined(separator: ","))
+                    temp = []
+                }
+                continue
+            }
+
+            if signal == 0 {
+                output.append(current)
+            }
+        }
+
+        return output.dropEmpty()
+    }
+
+    private func compare(_ lhs: [String], _ rhs: [String]) -> Int {
+        let zipped = zip(lhs, rhs)
+
+        for item in zipped {
+            if let left = Int(item.0), let right = Int(item.1) {
+                if left != right {
+                    return left - right
+                }
+            } else {
+                var left = item.0
+                var right = item.1
+
+                if Int(left) != nil {
+                    left = "[\(left)]"
+                }
+
+                if Int(right) != nil {
+                    right = "[\(right)]"
+                }
+
+                let sub = compare(unwrapList(left), unwrapList(right))
+
+                if sub != 0 {
+                    return sub
+                }
+            }
+        }
+
+        return lhs.count - rhs.count
+    }
+}
+
+private extension Array where Element: Collection {
+    func dropEmpty() -> [Element] {
+        filter { !$0.isEmpty }
     }
 }
--- Tests/advent-of-code-2022Tests/AOC_Tests_Day13.swift
+++ Tests/advent-of-code-2022Tests/AOC_Tests_Day13.swift
@@ -15,24 +15,24 @@ final class AOC_Tests_Day13: XCTestCase {
     func test_sample_question1() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "sample_input"))
         let answer = try XCTUnwrap(solver.solve1(input: input))
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 13)
     }
 
     func test_sample_question2() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "sample_input"))
         let answer = try XCTUnwrap(solver.solve2(input: input))
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 140)
     }
 
     func test_real_question1() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "real_input"))
         let answer = try XCTUnwrap(solver.solve1(input: input))
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 5659)
     }
 
     func test_real_question2() throws {
         let input = try XCTUnwrap(try? fetchSampleData(day: day, filename: "real_input"))
         let answer = solver.solve2(input: input)
-        XCTAssertEqual(answer, 0)
+        XCTAssertEqual(answer, 22110)
     }
 }
