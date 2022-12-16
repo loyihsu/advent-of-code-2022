61e0c4cc5c806fec99d6e4b3885f097bc6a89d71
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-12-16 10:27:58 +0900

    feat: add solution for day 15 part 2

--- Sources/advent-of-code-2022/Solutions/Day 15.swift
+++ Sources/advent-of-code-2022/Solutions/Day 15.swift
@@ -5,6 +5,8 @@
 //  Created by Yu-Sung Loyi Hsu on 2022/12/4.
 //
 
+import Foundation
+
 class Day15 {
     func solve1(input: String, caringRow: Int) -> Int {
         let points = handleInput(input)
@@ -16,7 +18,52 @@ class Day15 {
         return Set(points).count
     }
 
-    func solve2(input _: String) {}
+    func solve2(input: String, range: ClosedRange<Int>) -> Int {
+        var casting = [[ClosedRange<Int>]](repeating: [], count: range.count)
+        let rules = handleInput(input)
+
+        for rule in rules {
+            for idx in casting.indices {
+                let distance = rule.sensor.distance(rule.closest)
+                if let cast = rule.sensor.cast(on: idx, with: distance, limitingRange: range) {
+                    casting[idx].append(cast)
+                }
+            }
+        }
+
+        casting = casting.map { line in
+            var sorted = line.sorted {
+                $0.lowerBound < $1.lowerBound
+            }
+
+            var buffer: [ClosedRange<Int>] = []
+            var output = [ClosedRange<Int>]()
+
+            while let first = sorted.popFirst() {
+                if buffer.count < 2 {
+                    buffer.append(first)
+                } else if buffer.count == 2 {
+                    if let merged = buffer[0].merging(with: buffer[1]) {
+                        buffer = [merged, first]
+                    } else {
+                        output.append(buffer.popFirst()!)
+                    }
+                }
+            }
+
+            if buffer.count == 2, let merged = buffer[0].merging(with: buffer[1]) {
+                buffer = [merged]
+            }
+
+            output.append(contentsOf: buffer)
+            return output
+        }
+
+        let found = casting.enumerated()
+            .filter { $1.count > 1 }
+
+        return (found[0].element[0].upperBound + 1) * 4_000_000 + found[0].offset
+    }
 
     private func handleInput(_ input: String) -> [(sensor: Coordinate, closest: Coordinate)] {
         input
@@ -62,6 +109,14 @@ private struct Coordinate: Hashable {
         return xDistance + yDistance
     }
 
+    func cast(on row: Int, with distance: Int, limitingRange: ClosedRange<Int>) -> ClosedRange<Int>? {
+        guard row >= y - distance else { return nil }
+        guard row <= y + distance else { return nil }
+        let first = x + (distance - abs(y - row))
+        let second = x - (distance - abs(y - row))
+        return (min(first, second) ... max(first, second)).clamped(to: limitingRange)
+    }
+
     func expand(_ maxDistance: Int, existing: Coordinate, caringXRange: ClosedRange<Int>? = nil, caringYRange: ClosedRange<Int>? = nil) -> [Coordinate] {
         var xRange = (x - maxDistance) ... (x + maxDistance)
         var yRange = (y - maxDistance) ... (y + maxDistance)
@@ -85,5 +140,13 @@ private struct Coordinate: Hashable {
         .filter {
             $0 != existing
         }
+    }
+}
+
+private extension ClosedRange where Bound == Int {
+    func merging(with second: ClosedRange<Int>) -> ClosedRange<Int>? {
+        let newLowerBound = Swift.min(lowerBound, second.lowerBound)
+        let newUpperBound = Swift.max(upperBound, second.upperBound)
+        return overlaps(second) || abs(upperBound - second.lowerBound) == 1 ? newLowerBound ... newUpperBound : nil
     }
 }
--- Sources/advent-of-code-2022/main.swift
+++ Sources/advent-of-code-2022/main.swift
@@ -18,10 +18,10 @@ let result1 = solver.solve1(input: input, caringRow: 2_000_000)
 print("RESULT 1 -->")
 print(result1)
 
-let sample2 = solver.solve2(input: sample)
+let sample2 = solver.solve2(input: sample, range: 0 ... 20)
 print("SAMPLE 2 -->")
 print(sample2)
 
-let result2 = solver.solve2(input: input)
+let result2 = solver.solve2(input: input, range: 0 ... 4_000_000)
 print("RESULT 2 -->")
 print(result2)
