de283cce1809c4d247b4c84927cfc3b3b9bae929
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2022-12-24 18:58:51 +0800

    feat: add day 18 solutions

--- Sources/advent-of-code-2022/Solutions/Day 18.swift
+++ Sources/advent-of-code-2022/Solutions/Day 18.swift
@@ -6,11 +6,89 @@
 //
 
 class Day18 {
-    func solve1(input _: String) -> Int {
-        0
+    func solve1(input: String) -> Int {
+        let points = handleInputs(input: input)
+        return points
+            .map { point in
+                point
+                    .findAllNeighbours()
+                    .filter {
+                        !points.contains($0)
+                    }
+            }
+            .map(\.count)
+            .sum()
     }
 
-    func solve2(input _: String) -> Int {
-        0
+    func solve2(input: String) -> Int {
+        let input = handleInputs(input: input)
+
+        let lowerbound = input.findMin() - 1
+        let upperbound = input.findMax() + 2
+
+        var searching = [Dimension3(x: lowerbound, y: lowerbound, z: lowerbound)]
+        var visited = Set<Dimension3>()
+
+        var count = 0
+
+        while let first = searching.popLast() {
+            if !visited.contains(first) {
+                visited.insert(first)
+
+                for neighbour in first.findAllNeighbours() where neighbour.fullyWithin(lowerbound ... upperbound) {
+                    if input.contains(neighbour) {
+                        count += 1
+                    } else {
+                        searching.append(neighbour)
+                    }
+                }
+            }
+        }
+
+        return count
+    }
+
+    private func handleInputs(input: String) -> [Dimension3] {
+        input
+            .splitLines(shouldTrimWhitespacesAndNewlines: true)
+            .map {
+                $0.splitList(separator: ",", shouldTrimWhitespacesAndNewlines: true).compactMap(Int.init)
+            }
+            .map {
+                Dimension3(x: $0[0], y: $0[1], z: $0[2])
+            }
+    }
+}
+
+private struct Dimension3: Equatable, Hashable {
+    let x: Int
+    let y: Int
+    let z: Int
+
+    func findAllNeighbours() -> [Dimension3] {
+        [
+            Dimension3(x: x - 1, y: y, z: z),
+            Dimension3(x: x + 1, y: y, z: z),
+            Dimension3(x: x, y: y - 1, z: z),
+            Dimension3(x: x, y: y + 1, z: z),
+            Dimension3(x: x, y: y, z: z - 1),
+            Dimension3(x: x, y: y, z: z + 1),
+        ]
+    }
+
+    func fullyWithin(_ range: any RangeExpression<Int>) -> Bool {
+        [x, y, z].allSatisfy {
+            range.contains($0)
+        }
+    }
+}
+
+private extension Array where Element == Dimension3 {
+    func findMin() -> Int {
+        [map(\.x).min()!, map(\.y).min()!, map(\.z).min()!].min()!
+    }
+
+    func findMax() -> Int {
+        [map(\.x).max()!, map(\.y).max()!, map(\.z).max()!].max()!
     }
 }
--- Sources/advent-of-code-2022/main.swift
+++ Sources/advent-of-code-2022/main.swift
@@ -8,20 +8,20 @@
 let sample = try! fetchSampleData(filename: "Inputs/Sample")
 let input = try! fetchSampleData(filename: "Inputs/Input")
 
-let solver = Day15()
+let solver = Day18()
 
-let sample1 = solver.solve1(input: sample, caringRow: 10)
+let sample1 = solver.solve1(input: sample)
 print("SAMPLE 1 -->")
 print(sample1)
 
-let result1 = solver.solve1(input: input, caringRow: 2_000_000)
+let result1 = solver.solve1(input: input)
 print("RESULT 1 -->")
 print(result1)
 
-let sample2 = solver.solve2(input: sample, range: 0 ... 20)
+let sample2 = solver.solve2(input: sample)
 print("SAMPLE 2 -->")
 print(sample2)
 
-let result2 = solver.solve2(input: input, range: 0 ... 4_000_000)
+let result2 = solver.solve2(input: input)
 print("RESULT 2 -->")
 print(result2)
